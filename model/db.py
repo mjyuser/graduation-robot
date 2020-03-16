0e27d3895496aa9ff464fb463e8597d9fcd38015
皮默 <mojiayu@innotechx.com>
2020-03-16 22:22:02 +0800

    fix

--- model/db.py
+++ model/db.py
@@ -6,22 +6,27 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import redis
 
+try:
+    # 获取配置
+    Base = declarative_base()
+    config = configClient()
 
-# 获取配置
-Base = declarative_base()
-config = configClient()
+    # 实例化DB
+    database = config.database
+    schema = "mysql://%s:%s@%s/%s" % (database["user"], database["password"], database["host"], database["db"])
+    engine = create_engine(schema, encoding="utf8", echo=False)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# 实例化DB
-database = config.database
-schema = "mysql://%s:%s@%s/%s" % (database["user"], database["password"], database["host"], database["db"])
-engine = create_engine(schema, encoding="utf8", echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
+    res = engine.execute("select version()")
 
+    # 实例化Redis
+    redis_config = config.redis_config
+    redis_client = redis.Redis(host=redis_config['host'], port=redis_config['port'], db=redis_config['db'])
+except Exception as e:
+    print(e)
+    exit(1)
 
-# 实例化Redis
-redis_config = config.redis_config
-redis_client = redis.Redis(host=redis_config['host'], port=redis_config['port'], db=redis_config['db'])
 
 
 
